337cb60742c76af75fe11931a6b2fcf89f1e4c2a
Ck das <ckdas.bcs@gmail.com>
2019-11-20 11:10:03 +0000

    Reactapp setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "slessprnotes"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://d86pcmf582.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_snOFEQ13R",
+    APP_CLIENT_ID: "3jb0crf7b4rahc3mtlej0je3hl",
+    IDENTITY_POOL_ID: "us-east-1:07fd5abf-b82f-4f13-abb4-33aecbb2c359"
+  }
+};
